b5fca992ea3143f98aaf6bfdf9100b2a73fdc017
Shelby Clayton <shelby_clayton@hotmail.com>
2016-10-16 15:19:21 -0700

    will display name associated with username.

--- js/user-interface.js
+++ js/user-interface.js
@@ -7,8 +7,9 @@ var User = require('./../js/user.js').userModule;
 //   document.body.appendChild(img);
 // }
 
-var displayLogin = function(ghName, loginData) {
+var displayLogin = function(ghName, loginData, name) {
   $('.userLogin').text("Username: " + loginData);
+  $('.userName').text("Name: " + name);
 };
 
 $(document).ready(function() {
--- js/user.js
+++ js/user.js
@@ -5,7 +5,7 @@ User = function() {
 
 User.prototype.getRepos = function(ghName, displayUser) {
   $.get('https://api.github.com/users/' + ghName + '?access_token=' + apiKey).then(function(response) {
-    displayUser(ghName, response.login);
+    displayUser(ghName, response.login, response.name);
   }).fail(function(error) {
     $('.error').text("The information you are looking for could not be found. Please try a different search.");
   });
